de540406d53482712bc461f263bad2cfbb0fc274
Peter Garay-Robles <peter.garayrobles@gmail.com>
2025-10-09 23:40:27 -0400

    Update 10_9_2025 Seaborn.py

--- 10_9_2025 Seaborn.py
+++ 10_9_2025 Seaborn.py
@@ -11,3 +11,26 @@ sns.displot(us_salaries['salary'],
 
 # --------------------------------------------------------------------------
 
+# Joint Grid using KDE Plot
+joint = sns.JointGrid(data=subset,
+                      x="mintemp",
+                      y="windgustspeed")
+
+joint = joint.plot_joint(sns.kdeplot)
+
+
+joint = joint.plot_marginals(sns.kdeplot, 
+                             shade=True)
+plt.show()
+
+
+
+
+
+# --------------------------------------------------------------------------
+
+# Regression Plot for Categorical Variable
+sns.regplot(data=state_unemployment,
+            x="month",
+            y="unemployment_rate",
+            order=2)
