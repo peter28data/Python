d197704d6c0fc291c95feff4b3d5399dffb3e21d
Peter Garay-Robles <peter.garayrobles@gmail.com>
2025-10-13 23:49:57 -0400

    Update 10_4_2025_Writing Function.py

--- 10_4_2025_Writing Function.py
+++ 10_4_2025_Writing Function.py
@@ -8,11 +8,18 @@ def nth_rot(n):
 print(nth_root(3)(27))
 
 
+
+
+
 # ---------------------- Task 2: Return Largest from List --------------------------
 
 def sorted_elements(x, desc=True, n=2):
   new_x = sorted(x, reverse=desc)[0:n]
   return new_x
+
+
+
+
 
 # ---------------------- Task 3: Return Positive Numbers ----------------------------
 
