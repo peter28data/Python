0d3edbf8825baae0008e7a3aeb6b5c2ba427d686
Peter Garay-Robles <peter.garayrobles@gmail.com>
2025-10-13 23:49:45 -0400

    Update 10_4_2025_Writing Function.py

--- 10_4_2025_Writing Function.py
+++ 10_4_2025_Writing Function.py
@@ -19,5 +19,37 @@ def sorted_elements(x, desc=True, n=2):
 filter_positives = filter(lambda n: n > 0, x)
 print(list(filter_positives))
 
+
+
+
+
+# ----------------------- Task 4: Return Area & Perimeter ---------------------------------------------------
+
+def square(s):
+  """Returns area & perimeter of a square."""
+  try:
+    a = s * s
+    p = 4 * s
+    return a, p
+    expect TypeError:
+      print('s can not be a string')
+square('5')
+
+
+
+# --------------------- Task 5 : Convert Yards to Feet if feet is True -----------------------------------------------------
+
+def conver_yards(y, feet=True):
+  if feet is True:
+    new_y = y * 3
+  else:
+    new_y = y * 36
+  return new_y
+
+
+
+
+
 # --------------------------------------------------------------------------
+    
 
