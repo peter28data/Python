39b6c950b5ddd94d2b073e8b676fcdde009d8bb8
Peter Garay-Robles <peter.garayrobles@gmail.com>
2025-11-05 13:51:03 -0500

    Enhance visualizations with new plots
    Added polynomial regression and violin plot visualizations. Removed unnecessary plt.show() calls.

--- 10_9_2025 Seaborn.py
+++ 10_9_2025 Seaborn.py
@@ -8,7 +8,6 @@ sns.displot(us_salaries['salary'],
 
 
 
-
 # --------------------------------------------------------------------------
 
 # Joint Grid using KDE Plot
@@ -21,8 +20,6 @@ joint = joint.plot_joint(sns.kdeplot)
 
 joint = joint.plot_marginals(sns.kdeplot, 
                              shade=True)
-plt.show()
-
 
 
 
@@ -38,6 +35,13 @@ sns.regplot(data=state_unemployment,
 
 
 
+# Polynomial Regression
+sns.regplot(data=climate_change, 
+            x='relative_temp', 
+            y='co2', 
+            order=4)
+
+
 
 
 # -----------------------------------------------------------------
@@ -46,8 +50,6 @@ sns.relplot(x="age",
             y="rating",
             data=fifa_subset,
             kind="line")
-
-plt.show()
 
 
 
@@ -59,13 +61,26 @@ sns.countplot(x="nationality",
               hue="preferred_foot",
               data=fifa_chelsea)
 
-plt.show()
 
 
 
 # -----------------------------------------------------------------
 
+sns.violinplot(data=state_unemployment, 
+               y="Date",
+               x="Unemployment_Rate",
+               palette="Paired")
 
 
 
+
+# -----------------------------------------------------------------
+
+# Add Dashed Line
+fig, (ax0, ax1) = plt.subplots(nrows=1, ncols=2, sharey=True)
+sns.histplot(use_unemployment.Men, ax=ax0)
+Rsns.histplot(use_unemployment.Women, ax=ax1)
+ax1.axvline(x=5, label='Rate 5.0', linestyle='--')
+
+plt.show()
 
