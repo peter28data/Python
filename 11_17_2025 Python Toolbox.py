135889d347a28874c8144e73e7f730e708bc320c
Peter Garay-Robles <peter.garayrobles@gmail.com>
2025-11-18 23:28:56 -0500

    Create 11_17_2025 Python Toolbox.py

---
+++ 11_17_2025 Python Toolbox.py
@@ -0,0 +1,16 @@
+team_arrow = zip(code_names, characters_names)
+cnames, names = zip(*team_arrow)
+
+print(names)
+
+
+-----------------------------------------------------------------
+
+# Generator function that loads data in chunks
+def read_large_file(file_object):
+  """A generator function ro read a large file lazily."""
+  while True:
+    data = file_object.readline()
+    if not data:
+      break
+    yield data
